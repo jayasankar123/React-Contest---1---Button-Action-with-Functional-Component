48877cd64e4c03d38d4b0753e511dde774e42ec0
jayasankar123 <jayasankarreddy0@gmail.com>
2020-10-21 22:14:47 +0530

    functionalButtonClick

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [clicked, setClicked] = React.useState("");
+
+  let handleClick = () => {
+    setClicked(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        CLICK TO SEE PARA
+      </button>
+      {clicked ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 
 export default App;
